346773f0ab47556f7eaa4883d60d436ace1cff76
yousuf451 <yousufshaikh92@gmail.com>
2018-04-15 00:40:46 +0500

    Dynamic color change vue.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        description: 'Type any colour name, rgb, hex or hsl in the input below and see what happens !',
+        bgc:{
+            backgroundColor: ''
+        }
+    }
+})
