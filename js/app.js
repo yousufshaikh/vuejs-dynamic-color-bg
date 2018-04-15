680791336061ac175a16878a7826598a105356f5
yousuf451 <yousufshaikh92@gmail.com>
2018-04-15 20:04:11 +0500

    Jumbotron edited

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ var vm = new Vue({
     el: '#app',
     data: {
         description: 'Type any colour name, rgb, hex or hsl in the input below and see what happens !',
-        bgc:{
-            backgroundColor: ''
+        styleObj:{
+            color: ''
         }
     }
 })
